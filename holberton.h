f361e604e99f505d0c31aa5b8c6c5e26f8d4cd29
Matteo-lu <mateolondono.u@gmail.com>
2021-04-11 17:58:18 +0000

    header file version 1

---
+++ holberton.h
@@ -0,0 +1,34 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+
+/**
+ * struct list_s - Short description
+ * @str: pointer to separate PATH
+ * @next: Pointer to next node
+ *
+ * Description: this structure is of type linked list, to hold the separe PATH
+ */
+typedef struct list_s
+{
+	char *str;
+	struct list_s *next;
+} list_t;
+
+list_t *add_node_end(list_t **head, const char *str);
+char **buff_separator(char *str);
+char *conc_check_list(list_t **head, char *aux1);
+void free_list(list_t *head);
+int _strcmp(char *str1, char *str2);
+char *get_path(void);
+
+extern char **environ;
+
+#endif
